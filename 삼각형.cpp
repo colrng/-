fc83d1d05ae82fe6cf51c5d3072c6edfdbfb45ef
colrng <149384882+colrng@users.noreply.github.com>
2024-05-21 14:39:34 +0900

    Add files via upload

---
+++ 삼각형.cpp
@@ -0,0 +1,204 @@
+/**************************
+ * Includes
+ *
+ **************************/
+
+#include <windows.h>
+#include <gl/gl.h>
+
+
+/**************************
+ * Function Declarations
+ *
+ **************************/
+
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
+WPARAM wParam, LPARAM lParam);
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC);
+void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC);
+
+
+/**************************
+ * WinMain
+ *
+ **************************/
+
+int WINAPI WinMain (HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpCmdLine,
+                    int iCmdShow)
+{
+    WNDCLASS wc;
+    HWND hWnd;
+    HDC hDC;
+    HGLRC hRC;        
+    MSG msg;
+    BOOL bQuit = FALSE;
+    float theta = 0.0f;
+
+    /* register window class */
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) GetStockObject (BLACK_BRUSH);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "GLSample";
+    RegisterClass (&wc);
+
+    /* create main window */
+    hWnd = CreateWindow (
+      "GLSample", "OpenGL Sample", 
+      WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
+      0, 0, 512, 512,
+      NULL, NULL, hInstance, NULL);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL (hWnd, &hDC, &hRC);
+
+    /* program main loop */
+    while (!bQuit)
+    {
+        /* check for messages */
+        if (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage (&msg);
+                DispatchMessage (&msg);
+            }
+        }
+        else
+        {
+            /* OpenGL animation code goes here */
+
+            glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+            glClear (GL_COLOR_BUFFER_BIT);
+
+            glBegin(GL_TRIANGLES);
+			glColor3f(1.0f, 0.0f, 0.0f); // 빨간색
+			glVertex2f(0.0f, 0.5f);      // 꼭지점 1
+			glVertex2f(0.5f, -0.5f);     // 꼭지점 2
+			glVertex2f(-0.5f, -0.5f);    // 꼭지점 3
+			glEnd();
+            
+            glPushMatrix(); // 현재 변환 행렬을 저장합니다.
+    		glRotatef(theta, 0.0f, 0.0f, 1.0f); // theta 각도로 회전합니다.
+
+    		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // 윤곽선 모드로 설정합니다.
+    		glColor3f(1.0f, 1.0f, 1.0f); 
+    		
+    		glBegin(GL_QUADS);
+        	glVertex2f(0.5f, 0.5f);
+        	glVertex2f(0.5f, -0.5f);
+        	glVertex2f(-0.5f, -0.5f);
+        	glVertex2f(-0.5f, 0.5f);  
+   			glEnd();
+   			
+   			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // 채우기 모드로 되돌립니다.
+    		glPopMatrix();
+
+            SwapBuffers (hDC);
+
+            theta += 1.0f;
+            Sleep (1);
+        }
+    }
+
+    /* shutdown OpenGL */
+    DisableOpenGL (hWnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow (hWnd);
+
+    return msg.wParam;
+}
+
+
+/********************
+ * Window Procedure
+ *
+ ********************/
+
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
+                          WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+    case WM_CREATE:
+        return 0;
+    case WM_CLOSE:
+        PostQuitMessage (0);
+        return 0;
+
+    case WM_DESTROY:
+        return 0;
+
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_ESCAPE:
+            PostQuitMessage(0);
+            return 0;
+        }
+        return 0;
+
+    default:
+        return DefWindowProc (hWnd, message, wParam, lParam);
+    }
+}
+
+
+/*******************
+ * Enable OpenGL
+ *
+ *******************/
+
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC (hWnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory (&pfd, sizeof (pfd));
+    pfd.nSize = sizeof (pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | 
+      PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+    iFormat = ChoosePixelFormat (*hDC, &pfd);
+    SetPixelFormat (*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext( *hDC );
+    wglMakeCurrent( *hDC, *hRC );
+
+}
+
+
+/******************
+ * Disable OpenGL
+ *
+ ******************/
+
+void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC)
+{
+    wglMakeCurrent (NULL, NULL);
+    wglDeleteContext (hRC);
+    ReleaseDC (hWnd, hDC);
+}
